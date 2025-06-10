b492a08d4043351bde768a37c0c0aa925a8d8bfa
OptimalGrowthPartner <Jorge@optimalgrowthpartner.com>
2025-06-09 21:42:27 -0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Dashboard from './Dashboard';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Dashboard />);
