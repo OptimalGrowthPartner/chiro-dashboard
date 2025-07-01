3d0cdef83c92a2fde69259d4787691a317eb780f
OptimalGrowthPartner <Jorge@optimalgrowthpartner.com>
2025-06-30 22:17:16 -0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // Vite uses root/index.html by default - no changes needed
-});
